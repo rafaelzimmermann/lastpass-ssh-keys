3b8bc618cabfdee3f4b382233e8af6093f995b6d
Rafael Zimmermann <engzimmermann@gmail.com>
2019-05-30 22:25:35 +0200

    Use private key from last pass

--- ssh-agent-feeder.sh
+++ ssh-agent-feeder.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-lpass login
+set -e
 
-private_keys=$(find $HOME/.ssh/* -not -name "*.pub" -not -name "known_hosts")
+source .config
+
+lpass login $lastpass_username
 
 if ! ps -p $SSH_AGENT_PID > /dev/null; then
     eval `ssh-agent -s`
 fi
+unique_ids=$(lpass ls --color=never $lastpass_sshgroup | sed -nr 's/^.*id:.([0-9]+).*$/\1/p')
 
-for file_path in $private_keys; do
-    file_name=$(basename $file_path)
-    SSH_ASKPASS_FOR=$file_name DISPLAY=":0.0" SSH_ASKPASS="$PWD/ssh_askpass.sh" setsid ssh-add $file_path < /dev/null
+for unique_id in $unique_ids; do
+    private_key=$(lpass show --field="Private Key" $unique_id)
+    SSH_ASKPASS_FOR=$unique_id DISPLAY=":0.0" SSH_ASKPASS="$PWD/ssh_askpass.sh" setsid ssh-add - <<< "${private_key}"
 done
